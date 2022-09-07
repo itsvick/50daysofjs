12cb7340073931455bad6f816fefdddd35956c7e
Vivek kasture <vivek_kasture@techjoomla.com>
2022-09-07 14:25:19 +0530

    Added 2nd day challenge

---
+++ day2.js
@@ -0,0 +1,6 @@
+// Write a program to reverse a string. ( with / without using built in methods )
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("reverse me");
